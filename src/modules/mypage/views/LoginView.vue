ccd9f4f406d5b011ae3dca07cf6247ae2293c2ce
plusmail <plus4957@gmail.com>
2025-07-11 12:58:40 +0900

    데이터 로딩중 표시.

--- src/modules/mypage/views/LoginView.vue
+++ src/modules/mypage/views/LoginView.vue
@@ -3,9 +3,12 @@
     <div class="login-container">
       <h2>로그인</h2>
       <p>
-        관리자 로그인 아이디: admin, 비밀번호: !1aaaaaa
-        버스 운영자 로그인 아이디: bus1, 비밀번호: !1aaaaaa
-        사용자 로그인 아이디: user, 비밀번호: !1aaaaaa
+        관리자 아이디: admin
+            비밀번호: !1aaaaaa
+        버스 아이디: bus1
+            비밀번호: !1aaaaaa
+        사용자 아이디: user
+            비밀번호: !1aaaaaa
       </p>
 
       <!-- ─────────── 로그인 폼 ─────────── -->
